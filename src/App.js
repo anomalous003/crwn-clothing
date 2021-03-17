de6fabd0d185be584e5a88ddb6b39dc33edd6860
anomalous003 <luciferjenny553@gmail.com>
2021-03-17 17:55:36 +0530

    Shop Page (crwn-clothing)
    created and updated Shop Page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import './App.css';
 import HomePage from './pages/homepage/homepage-component.jsx'
+import { Route, Switch } from 'react-router-dom'
+import ShopPage from './pages/shop/shop-comoponent'
+
 
 function App() {
   return (
     <div>
-      <HomePage/>
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route exact path="/shop" component={ShopPage} />
+      </Switch>
     </div>
   );
 }
--- src/components/directory-menu/directory-menu-compnent.jsx
+++ src/components/directory-menu/directory-menu-compnent.jsx
@@ -9,35 +9,36 @@ class Directory extends Component {
             sections: [
                 {
                     title: 'hats',
-                    imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',
+                    imgUrl: 'https://i.ibb.co/cvpntL1/hats.png',
                     id: 1,
-                    linkUrl: 'shop/hats'
+                    linkUrl: 'hats'
+
                 },
                 {
                     title: 'jackets',
-                    imageUrl: 'https://i.ibb.co/px2tCc3/jackets.png',
+                    imgUrl: 'https://i.ibb.co/px2tCc3/jackets.png',
                     id: 2,
-                    linkUrl: 'shop/jackets'
+                    linkUrl: 'jackets'
                 },
                 {
                     title: 'sneakers',
-                    imageUrl: 'https://i.ibb.co/0jqHpnp/sneakers.png',
+                    imgUrl: 'https://i.ibb.co/0jqHpnp/sneakers.png',
                     id: 3,
-                    linkUrl: 'shop/sneakers'
+                    linkUrl: 'sneakers'
                 },
                 {
                     title: 'womens',
-                    imageUrl: 'https://i.ibb.co/GCCdy8t/womens.png',
+                    imgUrl: 'https://i.ibb.co/GCCdy8t/womens.png',
                     size: 'large',
                     id: 4,
-                    linkUrl: 'shop/womens'
+                    linkUrl: 'womens'
                 },
                 {
                     title: 'mens',
-                    imageUrl: 'https://i.ibb.co/R70vBrQ/men.png',
+                    imgUrl: 'https://i.ibb.co/R70vBrQ/men.png',
                     size: 'large',
                     id: 5,
-                    linkUrl: 'shop/mens'
+                    linkUrl: 'mens'
                 }
             ]
         }
@@ -47,7 +48,7 @@ class Directory extends Component {
         return (
             <div className='directory-menu'>
                 {
-                    this.state.sections.map(({ id, title, imageUrl, size }) => <MenuItem key={id} title={title} size={size} imgUrl={imageUrl} />)
+                    this.state.sections.map(({ id, ...otherSectionProps }) => <MenuItem key={id} {...otherSectionProps} />)
                 }
             </div>
         )
