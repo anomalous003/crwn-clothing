07e762f651281046ce3d7ab3505d99cc8d75998d
anomalous003 <luciferjenny553@gmail.com>
2021-04-03 18:34:35 +0530

    added payment feature with stripe

---
+++ src/components/stripe-button/stripe-button-component.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import StripeCheckout from 'react-stripe-checkout';
+
+const StripButton = ({ price }) => {
+  const onToken = token => {
+    console.log(token);
+    alert("Your Payment was Successful")
+  }
+
+  const priceForStripe = price * 100,
+    publishableKey = 'pk_test_51Ic7g0SHtgDZBOhuTLT6OOtFcEwQyjYvCqVCImWPE3z7atqL9MgcQ7UZuZHZ1ahgQtyYwIF6p55We4Mg5dwaGWzN00hWaEdhE7'
+
+  return (
+    <StripeCheckout
+      name='CRWN Clothing Ltd.'
+      description={`Your total is $${price}`}
+      label='Pay Now'
+      image='https://svgshare.com/i/CUz.svg'
+      shippingAddress
+      billingAddress
+      amount={priceForStripe}
+      token={onToken}
+      panelLabel={`Pay $${price}`}
+      stripeKey={publishableKey}
+    />
+
+  )
+}
+
+export default StripButton
--- src/pages/checkout/checkout-page-component.jsx
+++ src/pages/checkout/checkout-page-component.jsx
@@ -2,13 +2,14 @@ import React from 'react'
 import './checkout.styles.scss'
 
 import CheckoutItem from '../../components/checkout-item-component/checkout-item-component'
+import StripButton from '../../components/stripe-button/stripe-button-component'
 
 import { connect } from 'react-redux'
 import { selectCartItems, selectCartTotal } from '../../redux/cart/cart-selectors'
 import { createStructuredSelector } from 'reselect'
 
 const CheckoutPage = ({ cartItems, total }) => {
-return (
+  return (
     <div className='checkout-page'>
       <div className="checkout-header">
         <span className="header-block">Product</span>
@@ -17,10 +18,18 @@ return (
         <span className="header-block">Price</span>
         <span className="header-block"></span>
       </div>
-        {
-          cartItems.map(cartItem => <CheckoutItem key={cartItem.id} cartItem={cartItem}/>)
-        }
+      {
+        cartItems.map(cartItem => <CheckoutItem key={cartItem.id} cartItem={cartItem} />)
+      }
       <span className="total">TOTAL: ${total}</span>
+      <div className='test-warning'>
+        *Please use the following for test credit card payments*
+        <br/>
+        VISA: 4242 4242 4242 4242
+        <br />
+        Exp: 01/24, CVV: 123
+      </div>
+      <StripButton price={total}/>
     </div>
   )
 }
